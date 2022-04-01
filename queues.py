ac22d23e6505df103a92c8f88cf17c07fcb653cb
princenyamadi <princenyamadi.pn@gmail.com>
2022-04-01 14:47:27 +0000

    implementation of queues programmatically

---
+++ queues.py
@@ -0,0 +1,16 @@
+# FIFO
+# first in first out
+
+# enqueue - add
+# dequeue - remove
+
+from collections import deque
+
+data = []
+
+data.append(5)
+data.append(6)
+data.append(7)
+data.append(8)
+# dequeue element in front
+data.pop(0)
